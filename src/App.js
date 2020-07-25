a4a36ea6a376f19f5ffe5741c36348fdb7e0fe3c
ABritan <legenda.1011@mail.ru>
2020-07-25 13:04:07 +0700

    v1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-import UsersScreen from './screens/UsersScreen'
+import AutorizationScreen from './screens/AutorizationScreen/index'
 
 export default function App() {
-  return <UsersScreen />
+  return <AutorizationScreen />
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    flex: 1
+  }
 })

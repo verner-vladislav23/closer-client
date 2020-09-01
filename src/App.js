ed1e36ae5c6603206062b9211408007b465af898
Yrupka <legenda.1011@mail.ru>
2020-09-01 13:48:41 +0700

    Bad-navigation-fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react'
+import { AsyncStorage } from 'react-native'
 
 import { NavigationContainer, CommonActions } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+
 
 import UsersScreen from './screens/UsersScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import LoginScreen from './screens/LoginScreen'
 import SignUpScreen from './screens/SignUpScreen'
 import useGeolocation from './services/LocationService'
-import { AsyncStorage } from 'react-native'
-
-const Stack = createStackNavigator()
 
 // const loc = () => {
 //   const [error, position] = useGeolocation()
@@ -18,23 +17,25 @@ const Stack = createStackNavigator()
 //   console.log(error)
 //   console.log('ss')
 // }
+AsyncStorage.getItem('jwt').then(value => console.log(value))
+
+const Stack = createStackNavigator()
 
 const App = () => (
+  //todo error
   <NavigationContainer>
     <Stack.Navigator>
-      <Stack.Screen name='SignUpScreen' component={SignUpScreen} />
-      <Stack.Screen name='LoginScreen' component={LoginScreen} />
-      <Stack.Screen name='UsersScreen' component={UsersScreen} />
-      <Stack.Screen name='ProfileScreen' component={ProfileScreen} />
+      {AsyncStorage.getItem('jwt').then(value => value == null) ?
+        (<>
+          <Stack.Screen name='SignUpScreen' component={SignUpScreen} />
+          <Stack.Screen name='LoginScreen' component={LoginScreen} />
+        </>) :
+        (<>
+          <Stack.Screen name='UsersScreen' component={UsersScreen} />
+          <Stack.Screen name='ProfileScreen' component={ProfileScreen} />
+        </>)}
     </Stack.Navigator>
   </NavigationContainer>
 )
-
-// todo
-if (AsyncStorage.getItem('jwt') != null) {
-  CommonActions.navigate('UsersScreen')
-}
-//setTimeout(loc, 1000)
-//setInterval(loc, 2000)
 
 export default App
